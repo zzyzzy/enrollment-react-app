8ca7ac987a084051acdc531a5267f57fcfbd38e8
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-02 17:11:01 +0900

    state를 이용한 환영메세지 출력

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 import '../App.css';
 
 const EnrollmentForm = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        setWelcomeMessage(`환영합니다, ${firstName} ${lastName} 님!!`);
+        e.preventDefault();  // submit 기능 중지
+    };
     return(
         <div>
-            <form className="enrolForm">
+            <form className="enrolForm" onSubmit={handleSubmit}>
                 <h1>대학생 등록양식</h1>
                 <div><label>First Name</label>
-                    <input type="text" name="fname" /></div>
+                    <input type="text" name="fname"
+                    onBlur={(e) =>
+                            setFirstName(e.target.value)} /></div>
 
                 <div><label>Last Name</label>
-                        <input type="text" name="lname" /></div>
+                    <input type="text" name="lname"
+                    onBlur={(e) =>
+                        setLastName(e.target.value)}/></div>
 
                 <div><button type="submit">등록하기</button></div>
             </form>
+
+            <label id="studentMsg" className="message">
+                {welcomeMessage}
+            </label>
         </div>
     );
 };
